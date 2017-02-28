961eba7a0fbbceee81268e40d89d76d5ef324302
Jaime <jaime.viartola@gmail.com>
2017-02-28 08:20:22 +0100

    Commit inicial examen odoo
    Signed-off-by: Jaime <jaime.viartola@gmail.com>

---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Examen",
+
+    'summary': """examen"",
+    
+    'description': """
+        StarWars module to manage jedis:
+    """,
+
+    'icon': "/codigolimpio/static/img/jedi.png",
+
+
+    'author': "Jaime Lopez Viartola",
+    'website': "http://www.starwars.es/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Examen',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+    ],
+}
