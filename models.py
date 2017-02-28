3a2a5c069c932b9631e2eaed6297c6cb7c2802ad
Jaime <jaime.viartola@gmail.com>
2017-02-28 08:37:11 +0100

    Añadido ser vivo y especie
    Signed-off-by: Jaime <jaime.viartola@gmail.com>

--- models.py
+++ models.py
@@ -2,3 +2,16 @@
 
 from openerp import models, fields, api
 
+class Especie (models.Model):
+	_name = 'starWars.especie'
+
+	name = fields.Char(string="Nombre", required=True)
+
+	servivo_id = fields.One2many('starWars.servivo','especie_id', string="Ser Vivo")
+
+class Servivo (models.Model):
+	_name = 'starWars.servivo'
+
+	name = fields.Char(string="Nombre", required=True)
+
+	especie_id = fields.Many2one('starWars.especie', string="Especie")
