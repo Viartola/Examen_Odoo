fb499878f2cdcd768add49b517b0e57202cb0ed4
Jaime <jaime.viartola@gmail.com>
2017-02-28 09:20:03 +0100

    Añadidos sith y planetas
    Signed-off-by: Jaime <jaime.viartola@gmail.com>

--- models.py
+++ models.py
@@ -7,15 +7,16 @@ class Especie (models.Model):
 
 	name = fields.Char(string="Nombre", required=True)
 
-	servivo_id = fields.One2many('starWars.servivo','especie_id', string="Ser Vivo")
+	servivo_id = fields.One2many('examen.servivo','especie_id', string="Ser Vivo")
+	sith_id = fields.One2many('examen.sith', 'especie_id', string ="Sith")
 
 class Servivo (models.Model):
 	_name = 'examen.servivo'
 
 	name = fields.Char(string="Nombre", required=True)
 
-	especie_id = fields.Many2one('starWars.especie', string="Especie")
-	
+	especie_id = fields.Many2one('examen.especie', string="Especie")
+
 class Sith(models.Model):
 	_inherit = 'examen.servivo'
 	_name = 'examen.sith'
@@ -25,9 +26,28 @@ class Sith(models.Model):
 	afinidadOscuridad = fields.Integer(string="Afinidad a la oscuridad", readonly=True)
 	numeroSables = fields.Boolean(default=False)
 	colorSable = fields.Selection([
-	('rojo', "Rojo"),
-	('rojoOscuro', "Rojo Oscuro"),
-	])
+		('rojo', "Rojo"),
+		('rojoOscuro', "Rojo Oscuro"),
+		])
 
 	especie_id = fields.Many2one("examen.especie", string="Especie")
 
+	@api.one
+	def action_rojo(self):
+		self.colorSable = 'rojo'
+
+	@api.one
+	def action_rojoOscuro(self):
+		self.colorSable = 'rojoOscuro'
+
+	@api.onchange('rabia')
+	def set_afinity(self):
+		self.afinidadOscuridad = self.rabia * 2
+
+class Planeta(models.Model):
+	_name = 'examen.planeta'
+
+	name = fields.Char(string="Nombre", required=True)
+	distancia = fields.Float(string="Distancia en Parsecs", required=True)
+	destruido = fields.Boolean(string="Destruido por la estrella de la muerte", default=False)
+	destruidoDate = fields.Date(string="Fecha en la que fue destruido")
